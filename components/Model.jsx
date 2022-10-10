7f99608b042839a38ee066b7d4adfce3138366a4
parichay-batra <parichay.batra@mylofamily.com>
2022-10-10 14:10:07 +0530

    New 3 D Model

--- components/Model.jsx
+++ components/Model.jsx
@@ -2,10 +2,10 @@ import "@google/model-viewer";
 const Model = () => (
   <div id="card">
     <model-viewer
-      src="/COLETTE TOTE (BLACK)61080_GLTF.gltf"
+      src="/PBR - Metallic Roughness Alpha-test.gltf"
       ios-src=""
       //   poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
-      alt="A 3D model of an astronaut"
+      alt="A 3D model "
       shadow-intensity="1"
       camera-controls
       auto-rotate
