b8cb39f882d6125177acfe8ba744fd2de0f1d5bf
parichay-batra <parichay.batra@mylofamily.com>
2022-10-14 17:51:52 +0530

    new GLTF added

--- components/Model.jsx
+++ components/Model.jsx
@@ -2,7 +2,7 @@ import "@google/model-viewer";
 const Model = () => (
   <div id="card">
     <model-viewer
-      src="/PBR - Metallic Roughness Alpha-test.gltf"
+      src="/Diper_GLTF_2048.gltf"
       ios-src=""
       //   poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
       alt="A 3D model "
